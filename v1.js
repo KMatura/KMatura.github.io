b0eb2841d954152a92bd73e5af8902a8b7587acf
KMatura <matura.k04@htlwienwest.at>
2020-12-03 09:16:44 +0100

    final

---
+++ v1.js
@@ -0,0 +1,68 @@
+const numberCards = 16
+let openedCards = []
+let cardTypes=[]
+
+let counter = 0;
+
+for (let index = 0; index < numberCards/2; index++) {
+    cardTypes.push(index+1)
+    cardTypes.push(index+1)
+}
+cardTypes = shuffle(cardTypes)
+let p = document.querySelector('.deck')
+for (let index = 0; index < numberCards; index++) {
+        let c = document.createElement('div')
+        c.innerHTML = cardTypes[index]
+        c.type = cardTypes[index]
+        c.className="card"
+        c.addEventListener('click',flipp)
+        p.appendChild(c)
+}
+
+function flip(event){
+    if(!this.classList.contains('found'))
+        openCard(this)  
+}
+
+
+function openCard(c){
+    if(openedCards.length<2){
+        c.classList.toggle('flip')
+        openedCards.push(c)
+        if(openedCards.length==2){
+            if(openedCards[0].type == openedCards[1].type){
+                Score += 1
+                document.querySelector('#Score').textContent = `Score: ${Score}`;
+                window.setTimeout(
+                ()=>{
+                    openedCards.pop().classList.toggle('found')
+                    openedCards.pop().classList.toggle('found')
+                },
+                500    
+            )
+
+            }
+            else{             
+                window.setTimeout(
+                    ()=>{
+                        openedCards.pop().classList.toggle('flip')
+                        openedCards.pop().classList.toggle('flip')
+                    },
+                    1500
+                )
+                
+            }
+        }
+    }
+}
+function shuffle(arra1) {
+    var ctr = arra1.length, temp, index;
+    while (ctr > 0) {
+        index = Math.floor(Math.random() * ctr)
+        ctr--
+        temp = arra1[ctr]
+        arra1[ctr] = arra1[index]
+        arra1[index] = temp
+    }
+    return arra1;
+}
